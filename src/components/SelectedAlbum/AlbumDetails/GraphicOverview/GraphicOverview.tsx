b21b08e527e890c48cc221726688f5e1a6924f23
Gal Zohar <galz@ai21.com>
2022-06-09 00:07:35 +0300

    added randomizer to album graphic overview

--- src/components/SelectedAlbum/AlbumDetails/GraphicOverview/GraphicOverview.tsx
+++ src/components/SelectedAlbum/AlbumDetails/GraphicOverview/GraphicOverview.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import styled from 'styled-components';
+import { shuffle } from 'lodash';
 import { Album } from '../../../../common/dataTypes';
 import exampleGraphicOverview from '../../../../assets/exampleGraphicOveview.png';
+import exampleGraphicOverview2 from '../../../../assets/exampleGraphicOveview2.png';
+import exampleGraphicOverview3 from '../../../../assets/exampleGraphicOveview3.png';
+import exampleGraphicOverview4 from '../../../../assets/exampleGraphicOveview4.png';
 import { FadeInDiv } from '../../../Base/FadeInDiv';
 import { Characteristics } from './Characteristics/Characteristics';
 import {
@@ -15,10 +19,21 @@ interface GraphicOverviewProps {
 }
 
 export const GraphicOverview: FC<GraphicOverviewProps> = ({ album }) => {
+	const Image = useMemo(() => {
+		const images = [
+			exampleGraphicOverview,
+			exampleGraphicOverview2,
+			exampleGraphicOverview3,
+			exampleGraphicOverview4,
+		];
+		const selected = shuffle(images)[0];
+		return selected;
+	}, [album]);
+
 	return (
 		<GraphicOverviewContainer>
 			<Characteristics album={album} />
-			<GraphicOverviewImage src={exampleGraphicOverview} />
+			<GraphicOverviewImage src={Image} />
 		</GraphicOverviewContainer>
 	);
 };
