e19d4a46a9d7259a7f4ffe9bb10031454e08169b
Shani <shani.getz@gmail.com>
2022-06-11 15:11:14 +0300

    play color

--- src/components/SelectedAlbum/MusicPlayer/MusicPlayer.styles.tsx
+++ src/components/SelectedAlbum/MusicPlayer/MusicPlayer.styles.tsx
@@ -14,10 +14,12 @@ export const PlayButton = styled.img`
 	cursor: pointer;
 	margin-right: 14px;
 	width: 18px;
+	opacity: 0.8;
 `;
 
 export const SongName = styled.span`
-	font-family: 'Gotham';
+	font-family: 'Gotham-medium';
+	font-style: italic;
 	font-weight: 400;
 	font-size: 14px;
 	color: #ffffff;
@@ -26,11 +28,13 @@ export const SongName = styled.span`
 	white-space: nowrap;
 	text-overflow: ellipsis;
 	user-select: none;
+	opacity: 0.95;
 `;
 
 export const PlayerAnimation = styled.img`
 	width: 270px;
 	height: 70px;
+	opacity: 0.8;
 `;
 
 export const LeftSideContainer = styled.div`
